78f2e076cb4a793a5625f256c5ae1dd349b47de6
Michell Correia <michellascorreia@gmail.com>
2021-08-19 13:12:39 -0300

    Starting to implement Board and Piece

---
+++ src/boardlayer/Piece.java
@@ -0,0 +1,24 @@
+package boardlayer;
+
+public class Piece {
+    
+    protected Position position;
+    private Board board;
+
+
+    public Piece(){
+    }
+
+    // A posição de uma peça recém-criada é null.(não foi colocada no tabuleiro ainda.)
+    public Piece(Board board){
+        this.board = board;
+        position = null;
+    }
+
+    // Somente subclasses de Piece, e classes do mesmo pacote poderão acessar o tabuleiro da peça
+    protected Board getBoard() {
+        return board;
+    }
+      
+    //Não há setBoard pois as peças não poderão ser transferidas de tabuleiro.
+}
